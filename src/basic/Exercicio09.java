429b71d3c4d9bb3ef865ea74b272082f2b3476e7
Gabriel Picagevicz <gabrielpicagevicz15@gmail.com>
2022-07-19 20:57:35 -0300

    Exercicio 09

---
+++ src/basic/Exercicio09.java
@@ -0,0 +1,22 @@
+package basic;
+
+import java.util.Scanner;
+
+public class Exercicio09 {
+    public static void main(String[] args) {
+        //9. Faça um algoritmo que calcule e apresente o valor do volume de uma lata de óleo, dado
+        //seu raio e sua altura.
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o raio");
+        double raio = sc.nextDouble();
+        System.out.println("Digite o Altura");
+        double altura = sc.nextDouble();
+
+        double volume = 3.14 * raio * altura;
+
+        System.out.println("O volume é: "+volume);
+
+    }
+}

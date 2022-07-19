12ddc117460f287aa9cf08084d0101fcc062f4aa
Gabriel Picagevicz <gabrielpicagevicz15@gmail.com>
2022-07-19 19:52:09 -0300

    Treinando git e Exercicio 2

---
+++ src/basic/Exercicio02.java
@@ -0,0 +1,21 @@
+package basic;
+
+import java.util.Scanner;
+
+public class Exercicio02 {
+    public static void main(String[] args) {
+        //2. Dado o tamanho do lado de um quadrado, calcular a área e o perímetro do mesmo.
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o tamanho do lado do quadrado: ");
+        double lado = sc.nextDouble();
+
+        double area = (lado * lado);
+        double perimetro = (lado + lado + lado + lado);
+
+        System.out.println("A área do quadrado é: "+area+ "O perímetro do mesmo é: "+perimetro);
+
+
+    }
+}

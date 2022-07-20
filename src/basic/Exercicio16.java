17b6018b33c0287bee75e20f4af24fe5eb6dcc2a
Gabriel Picagevicz <gabrielpicagevicz15@gmail.com>
2022-07-20 20:46:30 -0300

    Exercicio 16

---
+++ src/basic/Exercicio16.java
@@ -0,0 +1,17 @@
+package basic;
+
+import java.util.Scanner;
+
+public class Exercicio16 {
+    public static void main(String[] args) {
+//        16. Escreva um programa para gerar o invertido de um número com três algarismos
+//                (exemplo: o invertido de 498 é 894).
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o número: ");
+        double numero = sc.nextDouble();
+
+        
+    }
+}

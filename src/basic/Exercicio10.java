a45396af430c307a972ab2e0ca0a04f50ca0c18e
Gabriel Picagevicz <gabrielpicagevicz15@gmail.com>
2022-07-19 21:08:47 -0300

    Exercicio 10

---
+++ src/basic/Exercicio10.java
@@ -0,0 +1,18 @@
+package basic;
+
+import java.util.Scanner;
+
+public class Exercicio10 {
+    public static void main(String[] args) {
+        //10. Converter um inteiro informado menor que 32 para sua representação em binário
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informe um inteiro menor que 32");
+        int numero = sc.nextInt();
+
+        
+
+
+    }
+}

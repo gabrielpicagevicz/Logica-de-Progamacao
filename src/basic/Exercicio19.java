34e31321cfdde5b441a6151720ca41d170b2d300
Gabriel Picagevicz <gabrielpicagevicz15@gmail.com>
2022-07-23 14:20:07 -0300

    Exercicio19

---
+++ src/basic/Exercicio19.java
@@ -0,0 +1,20 @@
+package basic;
+
+import java.util.Scanner;
+
+public class Exercicio19 {
+    public static void main(String args[]){
+        //19. Escreva um programa que calcule a raiz de uma equação do primeiro grau.
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o valor a");
+        double a = sc.nextDouble();
+
+        System.out.println("Digite o valor de b");
+        double b = sc.nextDouble();
+
+        double x = (a + b) ;
+
+
+    }
+}
